3c2c1bc3c9e2d008cd7fd05a54419969c7c0bb4c
Florin Asavei <asaveiflorin@gmail.com>
2020-03-24 23:41:24 +0200

    more demos

---
+++ Playground/SyntacticSugar/01_TypeSystem/01_TypesRecap/TypesRecap_End/BoolTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TypesRecap_End
+{
+    class BoolTest
+    {
+        public void BoolDemo()
+        {
+            bool a = true;
+
+            // we will se more when playing with operators.
+        }
+    }
+}
---
+++ Playground/SyntacticSugar/02_Operators/03_MemberAccessOperators/01_RangeOperator/RangeOperator_End/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RangeOperator_End
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] techArray = { "C", "C++", "C#", "F#", "JavaScript", "Angular", "TypeScript", "React", "GraphQL" };
+
+            foreach (var item in techArray[1..4])
+            {
+                Console.WriteLine(item);  //C++ C# F#
+            }
+        }
+    }
+}
